4ed43a07eb6f1feabe3b220a623f5f976137506e
Valeriu Birlaeanu <valeriu.birladeanu@gmail.com>
2025-05-23 19:27:37 +0300

    Deleting an unnecessary method from the basepage

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,9 +6,3 @@ class BasePage:
 
     def wait_for_element(self, selector: str, timeout: int = 5000):
         self.page.locator(selector).wait_for(state="visible", timeout=timeout)
-
-    def get_text(self, selector: str, wait_before: bool = False) -> str:
-        self.wait_for_element(selector)
-        if wait_before:
-            self.page.wait_for_timeout(1000)
-        return self.page.locator(selector).inner_text().strip()
